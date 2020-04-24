ff38a2634889402c9f19f2dbc03f1fc4b054cbb4
UmiKousaka <mr.ham1028@gmail.com>
2020-04-24 18:53:11 +0900

    UIテスト

---
+++ Assets/Audio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    AudioSource m_AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(m_AudioSource.time);
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject  m_Image;
+
+    [SerializeField]
+    GameObject m_Canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject prefab = (GameObject)Instantiate(m_Image);
+        prefab.transform.SetParent(m_Canvas.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

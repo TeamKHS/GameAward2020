e34a1a9b0a006c528df17f855206d65c1f510361
Seki.Hiroto <hiropaku0414@yahoo.co.jp>
2020-05-22 19:35:39 +0900

    線形補間

--- Assets/Scripts/Game/Judgement.cs
+++ Assets/Scripts/Game/Judgement.cs
@@ -21,63 +21,8 @@ public class Judgement : MonoBehaviour
 
     }
 
-
-    public void PlayerJudgement(Vector3 position, int index)
+    public void Hole()
     {
-        int value = 0;
 
-        switch (index)
-        {
-            case 0:
-                // →
-                value = 1;
-
-                break;
-
-            case 1:
-                // ←
-                value = -1;
-                break;
-
-            case 2:
-                // ↑
-                value = -m_Map.m_MapX;
-                break;
-
-            case 3:
-                // ↓
-                value = m_Map.m_MapX;
-                break;
-        }
-
-        int[] map = m_Map.GetMap;
-        int playerIndex = m_Map.GetMapIndex(position);
-        bool hit = false;
-
-        for (int i = 0; !hit; i++)
-        {
-            int nextIndex = playerIndex + i * value;
-
-            if (map[nextIndex] == (int)Map.MapType.Floor)
-            {
-                continue;
-            }
-            
-            switch (map[nextIndex])
-            {
-                case (int)Map.MapType.Wall:
-
-                    break;
-
-                case (int)Map.MapType.Hole:
-
-                    break;
-
-                case (int)Map.MapType.Goal:
-
-                    break;
-            }
-            break;
-        }
     }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -5,137 +5,142 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Map m_Map;
+    private Judgement m_Judgement;
 
-    Vector3 direction;
-    public AudioClip sound;
-    AudioSource audioSource;
-    const int bpm = 60;
-    int frame = 0;
+    private bool m_Move;
+    public bool IsMove
+    {
+        get { return m_Move; }
+        set { m_Move = value; }
+    }
 
-    Map m_Map;
-    GameObject respone;
+    // 線形補間用
+    private Vector3 m_StartPosition;
+    private Vector3 m_EndPosition;
+    private float m_Weight;
+    private float m_Value;
 
-    GameObject bgm;
-    Sound soundScript;
+    public Vector3 StartPosition
+    {
+        get { return m_StartPosition; }
+        set { m_StartPosition = value; }
+    }
+    public Vector3 EndPosition
+    {
+        get { return m_EndPosition; }
+        set { m_EndPosition = value; }
+    }
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-
         {
             GameObject obj = GameObject.Find("StageManager");
             StageManager stageManager = obj.GetComponent<StageManager>();
             m_Map = stageManager.Map;
         }
-
-
-        respone = GameObject.Find("Respone");
-
-        bgm = GameObject.Find("Sound");
-        soundScript = bgm.GetComponent<Sound>();
-    }
-
-
-    async void Respone(int Index)
-    {
-        await Task.Delay(1000);
-        int[] map = m_Map.GetMap;
-
-        if (map[Index] == (int)Map.MapType.Hole)
         {
-            this.transform.position = respone.transform.position;
+            GameObject obj = GameObject.Find("Judgement");
+            m_Judgement = obj.GetComponent<Judgement>();
         }
+
+        m_Move = false;
+        m_Weight = 0.0f;
+        m_Value = 0.01f;
     }
 
     void Update()
     {
-        // マップ情報
-        int[] map = m_Map.GetMap;
-        int mapX = m_Map.m_MapX;
-        int mapY = m_Map.m_MapY;
-
-
-        bool move = false;
-        int dir = 0;
-
-        if (Input.GetKeyUp(KeyCode.RightArrow))
+        // 動いている
+        if (m_Move)
         {
-            dir = 1;
-            move = true;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            dir = -1;
-            move = true;
-        }
-        if (Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            dir = -mapX;
-            move = true;
-        }
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            dir = mapX;
-            move = true;
-        }
-
-        // オレの位置
-        int index = m_Map.GetMapIndex((Vector2)this.transform.position);
-
-        for (int i = 1; move; i++)
-        {
-            int nextIndex = index + i * dir;
-            Vector3 nextPosition;
-
-            if (map[nextIndex] == (int)Map.MapType.Floor || map[nextIndex] == (int)Map.MapType.Non)
+            Vector3 nextPosition = new Vector3();
+            nextPosition = m_StartPosition * (1.0f - m_Weight) + m_EndPosition * m_Weight;
+            if ((m_Weight += m_Value) >= 1.0f)
             {
-                continue;
+                SetPlayerPosition(m_EndPosition);
+                m_Move = false;
+            }
+            else
+            {
+                SetPlayerPosition(nextPosition);
+            }
+        }
+        else
+        {
+            // マップ情報
+            int[] map = m_Map.GetMap;
+            int mapX = m_Map.m_MapX;
+            int mapY = m_Map.m_MapY;
+
+            bool move = false;
+            int dir = 0;
+
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                dir = 1;
+                move = true;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                dir = -1;
+                move = true;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                dir = -mapX;
+                move = true;
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                dir = mapX;
+                move = true;
             }
 
-            switch (map[nextIndex])
+            // オレの位置
+            int index = m_Map.GetMapIndex((Vector2)this.transform.position);
+
+            for (int i = 1; move; i++)
             {
-                case (int)Map.MapType.Wall:
-                    nextIndex -= dir;
+                int nextIndex = index + i * dir;
+                Vector3 nextPosition;
+                m_Move = true;
 
-                    // 配列番号から位置を算出
-                    nextPosition = m_Map.GetMapPosition(nextIndex);
+                if (map[nextIndex] == (int)Map.MapType.Floor || map[nextIndex] == (int)Map.MapType.Non)
+                {
+                    continue;
+                }
 
-                    // 位置更新
-                    this.transform.position = nextPosition;
+                switch (map[nextIndex])
+                {
+                    case (int)Map.MapType.Wall:
+                        nextIndex -= dir;
 
-                    break;
+                        // 配列番号から位置を算出
+                        m_StartPosition = m_Map.GetMapPosition(index);
+                        m_EndPosition = m_Map.GetMapPosition(nextIndex);
+                        m_Weight = 0.0f;
+                        break;
 
-                case (int)Map.MapType.Hole:
-                    // 配列番号から位置を算出
-                    nextPosition = m_Map.GetMapPosition(nextIndex);
+                    case (int)Map.MapType.Hole:
+                        // 配列番号から位置を算出
+                        nextPosition = m_Map.GetMapPosition(nextIndex);
 
-                    // 位置更新
-                    this.transform.position = nextPosition;
+                        break;
 
-                    break;
+                    case (int)Map.MapType.Goal:
+                        // 配列番号から位置を算出
+                        nextPosition = m_Map.GetMapPosition(nextIndex);
 
-                case (int)Map.MapType.Goal:
-                    // 配列番号から位置を算出
-                    nextPosition = m_Map.GetMapPosition(nextIndex);
-
-                    // 位置更新
-                    this.transform.position = nextPosition;
-
-                    break;
+                        break;
+                }
+                break;
             }
-            break;
         }
-
-
-
-
-        // ゴールだったら
-        if (map[index] == (int)Map.MapType.Goal)
-        {
-            Debug.Log("それってゴールかい？");
-        }
-
-        Respone(index);
     }
 
+    private void SetPlayerPosition(Vector3 position)
+    {
+        this.transform.position = position;
+    }
 }

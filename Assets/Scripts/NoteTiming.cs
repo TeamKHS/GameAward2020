e973c9aac0b57386867957eff9e5eae2467606ea
Seki.Hiroto <hiropaku0414@yahoo.co.jp>
2020-06-11 20:44:16 +0900

    音合わせﾁｶﾚﾀ...

--- Assets/Scripts/NoteTiming.cs
+++ Assets/Scripts/NoteTiming.cs
@@ -32,31 +32,70 @@ public class NoteTiming : MonoBehaviour
         return timing;
     }
 
+    private float Timing(int offsetSection, int offsetBeats, float value, int section, int beats)
+    {
+        //if (section >= 24)
+        //{
+        //    if ((beats - 4) < 0)
+        //    {
+        //        beats += 4;
+        //        section -= 1;
+        //    }else
+        //    {
+        //        beats -= 4;
+        //    }
+        //}
+
+        float timing = 0.0f;
+
+        if (beats - offsetBeats < 0)
+        {
+            beats += offsetBeats;
+            section -= 1;
+        }
+        else
+        {
+            beats -= offsetBeats;
+        }
+        section = section - offsetSection;
+
+        timing += (section * 8.0f) * value;
+        timing += beats * value;
+
+        return timing;
+    }
+
     private void Stage01()
     {
+        float BPM = 159.6f;
+        float hatibu = 60.0f / BPM / 2.0f;
         //float offset = 4.5f;
         float offset = 0.0f;
 
-        // ここにタイミングを書き込む
         m_Timing.Add(offset + 6.8f);
-        m_Timing.Add(offset + 15.2f);
-        m_Timing.Add(offset + 18.7f);
-
-        //m_Timing.Add(offset + 3.0f);
-        //m_Timing.Add(offset + 10.5f);
-
-        //m_Timing.Add(offset + 11.0f);
-        //m_Timing.Add(offset + 12.5f);
-
-        //m_Timing.Add(15.0f);
-        //m_Timing.Add(20.0f);
-        //m_Timing.Add(25.0f);
-        //m_Timing.Add(30.0f);
-        //m_Timing.Add(35.0f);
-
-        //for (int i = 1;i<30;i++)
-        //{
-        //    m_Timing.Add(3.0f * i);
-        //}
+        m_Timing.Add(offset + 6.8f + Timing(4, 0, hatibu, 9, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 0, hatibu, 12, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 0, hatibu, 13, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 0, hatibu, 15, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 0, hatibu, 16, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 0, hatibu, 16, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 0, hatibu, 20, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 24, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 25, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 29, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 30, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 32, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 32, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 33, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 35, 2));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 36, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 36, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 37, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 38, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 40, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 40, 4));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 41, 0));
+        m_Timing.Add(offset + 6.8f + Timing(4, 4, hatibu, 43, 4));
+        m_Timing.Add(-1.0f);
     }
 }
